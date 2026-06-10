e512ee3618868264e71ad76af0ce2f9c0d1f6b4e
Gabriel Lucio <gabriellucio0908@gmail.com>
2026-06-10 11:25:47 -0300

    Prova somativa

--- LOGICA_PROGRAMAÇÂO/gabriel/PROJETOS/Biblioteca.py
+++ LOGICA_PROGRAMAÇÂO/gabriel/PROJETOS/Biblioteca.py
@@ -12,34 +12,27 @@ def emprestimo():
     nome = entry_nome.get()
     usuario = combo_usuario.get()
     livro = combo_livro.get()
-    dias = entry_dias.get()
+    dias = int(entry_dias.get())
     if nome == "" or usuario == "" or livro == "" or dias == "":
         messagebox.showerror("Erro", "Por favor, preencha todos os campos.")
 
-        if usuario == "Comunidade" and livro == "Raro":
-            messagebox.showerror("Erro", "Desculpe, livros raros não estão disponíveis para a comunidade.")
+    if usuario == "Comunidade" and livro == "Raro":
+        messagebox.showerror("Erro", "Desculpe, livros raros não estão disponíveis para a comunidade.")
 
-    if usuario == "Aluno" and dias > "14":
+    if usuario == "Aluno" and dias > 14:
         messagebox.showinfo("Observação","Acima de 14 dias tem uma taxa de 5 reais por dia extra")
         dias_extras = int(dias) - 14
         taxa = dias_extras * 5
         messagebox.showinfo("Taxa", f"Você tem uma taxa de R${taxa} por {dias_extras} dias extras.")
         messagebox.showinfo("Empréstimo Realizado", f"Olá {nome}, você é um {usuario} e escolheu o livro {livro} por {dias} dias e uma taxa de R${taxa}. Aproveite a leitura!")
 
-    else:
-         messagebox.showinfo("Empréstimo Realizado")
-
-    if usuario == "Comunidade" and dias > "7":
+    if usuario == "Comunidade" and dias > 7:
         messagebox.showinfo("Observação","Acima de 7 dias tem uma taxa de 5 reais por dia extra")
         dias_extras = int(dias) - 7
         taxa = dias_extras * 5
         messagebox.showinfo("Taxa", f"Você tem uma taxa de R${taxa} por {dias_extras} dias extras.")
-    else:
-        messagebox.showinfo("Empréstimo Realizado", f"Olá {nome}, você é um {usuario} e escolheu o livro {livro} por {dias} dias. Aproveite a leitura!")
 
-    if usuario == "Comunidade" and dias <= "7":
-        messagebox.showinfo("Empréstimo Realizado", f"Olá {nome}, você é um {usuario} e escolheu o livro {livro} por {dias} dias. Aproveite a leitura!")
-    else:
+    if usuario == "Comunidade" and dias <= 7:
         messagebox.showinfo("Empréstimo Realizado", f"Olá {nome}, você é um {usuario} e escolheu o livro {livro} por {dias} dias. Aproveite a leitura!")
 
 
