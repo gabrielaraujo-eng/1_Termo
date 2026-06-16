978add52e6bab3b6acb889b63266054a424b8b45
gabrielaraujo-eng <gabriel.l.araujo@edu.senai.br>
2026-06-16 16:20:56 -0300

    Update pi_formula.py

--- LOGICA_PROGRAMAÇÂO/gabriel/Atividades_complementares/Python/pi_formula.py
+++ LOGICA_PROGRAMAÇÂO/gabriel/Atividades_complementares/Python/pi_formula.py
@@ -13,23 +13,28 @@ pi_arredondado = round(math.pi, casas)
 print("Pi arredondado:", pi_arredondado)
 
 #outro jeito sem import
-valor = input("Gostaria de calcular o valor de pi?(s/n): ")
 
-if valor == "s":
-    print("pi = c / d")
-    c = int(input("Digite o valor de c(circunferência): "))
-    d = int(input("Digite o valor de d(diâmetro): "))
-    if d == 0:
-        print("divisão por zero nao é possivel")
+while True:
+    valor = input("Gostaria de calcular o valor de pi?(s/n): ")
+    
+    if valor == "s":
+        print("pi = c / d")
+        c = int(input("Digite o valor de c(circunferência): "))
+        d = int(input("Digite o valor de d(diâmetro): "))
+        if d == 0:
+            print("divisão por zero nao é possivel")
+            break
+        else:
+            pi = c / d
+            print("Valor de pi:", pi)
+            casas = int(input("Quantas casas decimais você quer arredondar? ")) 
+            pi_arredondado = round(pi, casas)
+            print("Pi arredondado:", pi_arredondado)
+    
+    elif valor == "n":
+        print("OK")
+        break
+    
     else:
-        pi = c / d
-        print("Valor de pi:", pi)
-        casas = int(input("Quantas casas decimais você quer arredondar? ")) 
-        pi_arredondado = round(pi, casas)
-        print("Pi arredondado:", pi_arredondado)
-
-elif valor == "n":
-    print("OK")
-
-else:
-    print("resposta foi diferente de sim e nao")
+        print("resposta foi diferente de sim e nao")
+        break
